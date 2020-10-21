ec1962eea7dfec0b81fc4f8a0ce3d7928d1af939
Cecilia <cbarkume@dev-10.com>
2020-10-21 11:38:13 -0500

    Added sheep related methods

--- src/main/java/learn/hodgepodge/HodgepodgeController.java
+++ src/main/java/learn/hodgepodge/HodgepodgeController.java
@@ -2,6 +2,7 @@ package learn.hodgepodge;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -9,6 +10,8 @@ import java.time.LocalDateTime;
 
 @RestController
 public class HodgepodgeController {
+
+    private static int sheepCount = 0;
 
     @GetMapping ("/name")
     public String getName(){
@@ -23,6 +26,21 @@ public class HodgepodgeController {
     @GetMapping ("/greet/{name}")
     public String greet(@PathVariable String name){
         return "Hello " + name + "!";
+    }
+
+    @PutMapping ("/sheep")
+    public void countingSheep() {
+        sheepCount++;
+    }
+
+    @GetMapping ("/sheep")
+    public int howManySheep() {
+        return sheepCount;
+    }
+
+    @PutMapping ("/sheep/{amount}")
+    public void incSheepByAmount(@PathVariable int amount) {
+        sheepCount += amount;
     }
 
 }
